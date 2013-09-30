ad6b655dbc5d2745234805dcdcf07b7528d51ae3
Neil Gehani <njgonline@me.com>
2013-09-29 20:17:27 -0700

    Mailing List

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ LearnRails::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   resources :contacts, only: [:new, :create]
+  resources :visitors, only: [:new, :create]
   root to: 'visitors#new'
 
   # You can have the root of your site routed with "root"
